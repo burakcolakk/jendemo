ade05c33046227bc9642f7c950a4278b8e3fc644
Burak Çolak <35burakcolak@gmail.com>
2025-12-25 09:48:54 +0300

    jenkins test projesi hazır

---
+++ demo/src/test/java/com/burak/demo/WebsiteAccessTest.java
@@ -0,0 +1,30 @@
+package com.burak;
+
+import org.junit.jupiter.api.Test;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WebsiteAccessTest {
+    @Test
+    public void testUrlAccess() {
+        try {
+            // Kontrol edilecek adres
+            URL url = new URL("https://example.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.connect();
+
+            int code = connection.getResponseCode();
+            System.out.println("Site Durum Kodu: " + code);
+
+            // Eğer 200 değilse test FAIL olur
+            assertEquals(200, code, "Siteye erişilemedi!");
+
+        } catch (Exception e) {
+            // Bağlantı hatası olursa testi FAIL yap
+            org.junit.jupiter.api.Assertions.fail("Bağlantı hatası: " + e.getMessage());
+        }
+    }
+}
